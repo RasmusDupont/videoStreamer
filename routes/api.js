22ab041ef97263e22d2a3890d7b967440534e6ca
Rasmus <rasmusduponts@gmail.com>
2019-04-08 21:26:19 +0200

    video route works + add node for heroku

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+const videoRouter = require('./video/video.js');
+
 apiRouter.get("/", function (req, res) {
     res.send("Video Streamer API")
 });
+
+apiRouter.use('/video', videoRouter);
 
 module.exports = apiRouter;
--- routes/video/video.js
+++ routes/video/video.js
@@ -1,19 +1,29 @@
-let fs      = require('fs')
-let path    = require('path');
-let express = require('express');
-let router  = express.Router();
+const fs = require('fs')
+const express = require('express');
+const router  = express.Router();
 
 router.get('/', function(req, res, next) {
 
-	let file = '';
+	console.log("OK /");
+	res.sendStatus(200);
+});
+
+router.get('/japan-mp4', function(req, res, next) {
+
+	console.log("OK /japan-mp4");
+
+	let file = 'assets/why_is_it_so_easy_to_be_thin_in_japan_1080p.mp4';
 
 	//Get file metadata
 	fs.stat(file, function(err, stats) {
 		if (err) {
 			if(err.code === 'ENOENT') {
+				console.log("File false");
 				return res.sendStatus(404);
 			}
 			return next(err)
+		} else {
+			console.log("File true");
 		}
         //Byterange - afsnit "Early termination & repositioning a video"
         //fx 56321 - 155991132
@@ -26,7 +36,7 @@ router.get('/', function(req, res, next) {
 			return next(err);
 		}
 	});
-
+	res.sendStatus(200);
 });
 
 module.exports = router;
